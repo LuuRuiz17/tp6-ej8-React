41db2806ca66bb2600957408c266457680643979
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-02 16:05:22 -0300

    Agregué el frmulario y le di estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BarraDeNavegacion from './BarraDeNavegacion';
+import Formulario from './Formulario';
+
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <header>
+        <BarraDeNavegacion></BarraDeNavegacion>
+      </header>
+      <main>
+        <h1 className='text-center color-loop titulo mt-4'>Formulario</h1>
+        <Formulario></Formulario>
+      </main>
+      <footer className='py-4 rosa text-center'>&copy; Todos los derechos reservados</footer>
     </>
   )
 }
---
+++ src/Formulario.jsx
@@ -0,0 +1,32 @@
+import { Form, Button } from "react-bootstrap";
+
+const Formulario = () => {
+    return (
+        <Form className="bg-form p-3 rounded mx-auto my-4">
+            <Form.Group className="mb-3 campoForm" controlId="formNombre">
+                <Form.Label className="label-color">Nombre</Form.Label>
+                <Form.Control className="input-color" type="text" placeholder="Ej: Luciana" />
+            </Form.Group>
+            <Form.Group className="mb-3 campoForm" controlId="formApellido">
+                <Form.Label className="label-color">Apellido</Form.Label>
+                <Form.Control className="input-color" type="text" placeholder="Ej: Luciana" />
+            </Form.Group>
+            <Form.Group className="mb-3 campoForm" controlId="formDNI">
+                <Form.Label className="label-color">DNI</Form.Label>
+                <Form.Control className="input-color" type="number" placeholder="Ej: Luciana" />
+            </Form.Group>
+            <Form.Group className="mb-3 campoForm" controlId="formEmail">
+                <Form.Label className="label-color">Email address</Form.Label>
+                <Form.Control className="input-color" type="email" placeholder="Ej: lucianaruiz.934@gmail.com" />
+                {/* <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text> */}
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Enviar
+            </Button>
+        </Form>
+    );
+};
+
+export default Formulario;
